03f9088e30366a24b2136b8763e6ac99bff2d397
gboyce <gjboyce@gmail.com>
2017-07-10 22:21:08 -0500

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var path = require('path');
 var db = require('./models');
 
 var app = express();
-var PORT = (process.env.PORT || 3000);
+var PORT = (process.env.PORT || 8080);
 
 app.use(express.static(__dirname + "/public"));
 
